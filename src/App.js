8df1a3b1ba2954af4e60558a9c194b3cb0cacc8d
Ranjithkumar-M <ranvitranjit@gmail.com>
2020-01-12 12:30:04 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { TSQlik, QSApp, QSProps } from 'ts-qlik'
 import './App.css';
 
-function App() {
+function App(props) {
+  const [active, setActive] = useState('qlik');
+  const [qlik, setQlik] = useState(null);
+  const config = {
+    "host": "localhost",
+    "port": 80,
+    "prefix": "/",
+    "isSecure": false
+};
+
+  const changeTab = (data) => {
+    setActive(data);
+    if(data === 'app') {
+      QSApp('031ec68a-9247-4fc5-9ddd-3b1764924aab', config).then((q) => {
+        q.currApp.app.getObject('obj1', 'fNGRa')
+        q.currApp.app.getObject('obj2', 'JcJvj')
+      })
+    }
+  }
+
+  const callQlikFn = () => {  
+  TSQlik(config).then((q) => {
+    setQlik(q);
+  })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        TS-Qlik Application
+     </div>
+      { qlik && 
+            <div className="Content">
+              <div className="tabs">
+                <div onClick={() => changeTab('qlik')} className={(active === 'qlik') ? 'tab active' : 'tab'}>TSQlik function</div>
+                <div onClick={() => changeTab('app')} className={active === 'app' ? 'tab active' : 'tab'}>OpenApp function</div>
+              </div>
+              {active === 'qlik' && <div className="Qlik">
+                <div className="directoryName">Directory: {qlik.qlikUser.directory}</div>
+                <div className="userName">UserName: {qlik.qlikUser.userid}</div>
+              </div> }
+              {active === 'app' && <div className="OpenApp">
+                <div id="obj1" className="object"></div>
+                <div id="obj2" className="object"></div>
+              </div> }
+      </div> }
+      { !qlik && 
+            <div className="Content">
+              <div className="qlikBtn">
+                <button onClick={() => callQlikFn()}>Call Qlik</button>
+              </div>
+      </div> }
     </div>
   );
 }
